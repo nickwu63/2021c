556408d4a3275f5d27b2cddc5aff5ac49dd1ec2c
nick <91250501+nickwu63@users.noreply.github.com>
2021-10-28 17:46:32 +0800

    Add files via upload

---
+++ week07.cpp/week07-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;///1973發明c語言,1989年 ANSI C標準
+    for(i=0; i=3; i++){
+        printf("課本的寫法喔\n");
+    }
+    ///1999年/1998 的 c/c++的規定
+    for(int i=0; i<3; i++){
+        printf("小葉老師教的寫法喔\n");
+    }
+}
